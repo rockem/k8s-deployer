0dbc64175e1409a9ba9307e8ffb16ec631d4ac79
agur <amaziagur@gmail.com>
2017-01-19 13:08:21 +0200

    all test passed

--- features/blue_green_deployment.feature
+++ features/blue_green_deployment.feature
@@ -1,7 +1,13 @@
 Feature: dark deploy service to k8s
 
-  Scenario: healthy service still serving when sick service deployed
+#  Scenario: healthy service still serving when sick service deployed
+#    Given login
+#    When deploy healthy service
+#    And deploy sick service should fail
+#    Then healthy service still serving
+
+  Scenario: updated service
     Given login
-    When deploy healthy service
-    And deploy sick service should fail
-    Then healthy service still serving
+    When deploy hello:1 service
+    And deploy hello:2 service
+    Then service updated to version 2
--- features/steps/blue_green_deployment.py
+++ features/steps/blue_green_deployment.py
@@ -24,10 +24,12 @@ HEALTHY_SERVICE_IMAGE_NAME = AWS_REGISTRY_URI + '/' + HEALTHY_NAME
 def login(context):
     subprocess.check_output('$(aws ecr get-login --region us-east-1)', shell=True)
 
-@when("deploy (.*)? service(?: should (.*))?")
-def deploy_healthy_service(context, name, status):
+@when("deploy (.*?)(?:\\:(\d+))? service(?: should (.*))?")
+def deploy_healthy_service(context, name, version, status):
+    print 'name is %s' %name
+    print 'version is %s' %version
     image_name = __get_service_image_name(name)
-    __docker_build(image_name, "./features/%s/." % name)
+    __docker_build(version, image_name, "./features/%s/." % name)
     __upload_to_registry(name)
     assert __deploy(image_name) == (status != 'fail') , 'failed to deploy'
 
@@ -37,9 +39,23 @@ def healthy_service_is_serving(context):
     __wait_for_service_to_be_available_k8s()
 
 
+def __validate_version_updated(domain, version):
+    o = requests.get('http://' + domain + "/version")
+    print 'service version output %s' %o
+    assert json.loads(o.text)['version']== str(version), 'Healthy service not serving anymore'
+
+
+
+@then("service updated to version (\d+)")
+def service_updated(context, version):
+    __wait_for_service_deploy()
+    domain = __wait_for_service_to_be_available_k8s()
+    __validate_version_updated(domain, version)
+
 def __wait_for_service_to_be_available_k8s():
     service_up = False
     counter = 0
+    result = None
     while counter < 120:
         counter =+1
         output = subprocess.check_output("kubectl describe services %s" % HEALTHY_NAME, shell=True)
@@ -63,6 +79,8 @@ def __wait_for_service_to_be_available_k8s():
 
     if not service_up:
         raise Exception('The service in k8s probably did not start')
+
+    return result
 
 def __wait_for_service_deploy():
     counter = 0
@@ -98,6 +116,6 @@ def __deploy(name):
         "python deployer/deployer.py deploy --image_name %s --target %s "
         "--git_repository %s" % (name, TARGET_ENV, GIT_REPO)) == 0
 
-def __docker_build(name, location):
-    comand = "docker build -t %s %s" % (name.strip(), location)
+def __docker_build(version, name, location):
+    comand = "docker build --build-arg VERSION=%s -t %s %s" % (version, name.strip(), location)
     os.system(comand)
