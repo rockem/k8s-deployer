e1591ff87b3c7ced19cff326d48569247f4b3596
agur <amaziagur@gmail.com>
2017-06-26 12:43:43 +0300

    cleanup AppDriver

--- features/support/app.py
+++ features/support/app.py
@@ -1,10 +1,4 @@
-import json
-import re
 import time
-
-import subprocess
-
-import requests
 
 
 class AppDriver:
@@ -27,29 +21,5 @@ class AppDriver:
             time.sleep(1)
 
         return result
-
-
-    def __pod_running(self, image_name):
-        pod_name = self.__grab_pod_name(image_name)
-        match = re.search(r"Status:\s(.*)", self.__describe_pod(pod_name))
-        if match:
-            return match.group(1).strip() == 'Running'
-        else:
-            raise Exception('service %s has no pod!' % pod_name)
-
-
-    def __describe_pod(self, pod_name):
-        return self, __run("kubectl --namespace %s describe pods %s" % (NAMESPACE, pod_name))
-
-
-    def __grab_pod_name(self, pod_name):
-        output = self, __run("kubectl --namespace %s get pods" % (NAMESPACE))
-        list = output.split()
-        for item in list:
-            if item.startswith(pod_name):
-                return item
-
-    def __run(self, command):
-        return subprocess.check_output(command, shell=True)
 
 
