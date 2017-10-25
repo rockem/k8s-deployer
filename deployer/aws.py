7505b92ef720116cf3dbd2c2453b5da99e4ab48f
idavidov <idavidov@heed-global.com>
2017-10-25 15:13:47 +0300

    retry swagger

--- deployer/aws.py
+++ deployer/aws.py
@@ -1,7 +1,4 @@
-import subprocess
-
 import boto3 as boto3
-import time
 
 from log import DeployerLogger
 from util import EnvironmentVariablesFetcher
@@ -18,5 +15,15 @@ class ApiGatewayConnector(object):
         self.target_env = EnvironmentVariablesFetcher().fetch("TARGET_ENV")
 
     def upload_swagger(self, yml_path):
-        client.put_rest_api(restApiId=self.rest_api_id, mode='overwrite', failOnWarnings=False, body=SwaggerFileReader(yml_path).read())
-        client.create_deployment(restApiId=self.rest_api_id, stageName=self.target_env, cacheClusterEnabled=False)
+        client.put_rest_api(restApiId=self.rest_api_id, mode='overwrite', failOnWarnings=False,
+                            body=SwaggerFileReader(yml_path).read())
+        for i in range(0, 5):
+            try:
+                client.create_deployment(restApiId=self.rest_api_id, stageName=self.target_env,
+                                         cacheClusterEnabled=False)
+                break
+            except Exception as e:
+                time.sleep(2 * i + 1)
+                if i == 4:
+                    raise e
+                pass
