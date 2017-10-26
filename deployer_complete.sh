d74c3a6a866d0c0a31eb670251093c25d8032c1b
idavidov <idavidov@heed-global.com>
2017-10-26 15:39:36 +0300

    credentials

--- deployer_complete.sh
+++ deployer_complete.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-aws configure set aws_access_key_id $KEY_ID
-aws configure set aws_secret_access_key $ACCESS_KEY
+aws configure set region us-east-1
+aws configure set aws_access_key_id $ACCESS_KEY_ID
+aws configure set aws_secret_access_key $SECRET_KEY
 
 
 prefix="${TARGET_ENV%%:*}"
