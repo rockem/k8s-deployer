796c3113652bb93a09cc1eb87bb9c3b5ba000da9
alon-harel <mayalon.harel@gmail.com>
2018-06-06 13:20:32 +0300

    Chor/156839976 https to ui service (#51)

--- deployer/aws.py
+++ deployer/aws.py
@@ -6,9 +6,6 @@ from log import DeployerLogger
 from util import EnvironmentVariablesFetcher
 from yml import SwaggerFileReader
 
-apigateway_client = boto3.client('apigateway')
-acm_client = boto3.client('acm')
-
 logger = DeployerLogger('deployer').getLogger()
 
 
@@ -18,6 +15,7 @@ class ApiGatewayConnector(object):
         self.target_env = EnvironmentVariablesFetcher().fetch("TARGET_ENV")
 
     def upload_swagger(self, yml_path):
+        apigateway_client = boto3.client('apigateway')
         apigateway_client.put_rest_api(restApiId=self.rest_api_id, mode='overwrite', failOnWarnings=False,
                                        body=SwaggerFileReader(yml_path).read())
         for i in range(0, 5):
@@ -33,6 +31,7 @@ class ApiGatewayConnector(object):
 
 class AwsConnector(object):
     def get_certificate_for(self, domain):
+        acm_client = boto3.client('acm')
         certificates = acm_client.list_certificates()
         return next((certificate['CertificateArn']
                      for certificate in certificates['CertificateSummaryList']
